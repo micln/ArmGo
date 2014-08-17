19def67181120893446993cc8e93da19e2c2f5cf
amc <amc@AFC.local>
2014-08-18 02:17:38 +0800

    format

--- modules/arm.js
+++ modules/arm.js
@@ -25,13 +25,13 @@
 	}
 */
 //
-function ARM(){
+function ARM() {
 	this.leftz = state.x - conf.cell.x - 12;
 	this.topz = state.y;
 	this.bottonz = state.y + conf.cell.y * 12;
 	this.speed = 6;
 	this.running = false;
-	this.init = function(){
+	this.init = function() {
 		this.running = false;
 		this.x = this.leftz;
 		this.y = this.topz;
@@ -39,147 +39,146 @@ function ARM(){
 		this.hand = 0;
 		this.draw();
 	}
-	this.draw = function(){
+	this.draw = function() {
 		var x = this.x;
 		var y = this.y;
 		cxt.beginPath();
-		cxt.moveTo(x,y);
-		cxt.lineTo(x,y+30);
+		cxt.moveTo(x, y);
+		cxt.lineTo(x, y + 30);
 
-		cxt.lineTo(x+10,y+30);
-		cxt.lineTo(x+1,y+30+4);
-		cxt.lineTo(x-2,y+30+4);
-		cxt.lineTo(x-4,y+30);
+		cxt.lineTo(x + 10, y + 30);
+		cxt.lineTo(x + 1, y + 30 + 4);
+		cxt.lineTo(x - 2, y + 30 + 4);
+		cxt.lineTo(x - 4, y + 30);
 
-		cxt.lineTo(x-3,y+30);
-		cxt.lineTo(x-6,y+15);
-		cxt.lineTo(x-3,y);
+		cxt.lineTo(x - 3, y + 30);
+		cxt.lineTo(x - 6, y + 15);
+		cxt.lineTo(x - 3, y);
 
-		cxt.lineTo(x-4,y);
-		cxt.lineTo(x-2,y-4);
-		cxt.lineTo(x+1,y-4);
-		cxt.lineTo(x+10,y);
+		cxt.lineTo(x - 4, y);
+		cxt.lineTo(x - 2, y - 4);
+		cxt.lineTo(x + 1, y - 4);
+		cxt.lineTo(x + 10, y);
 
 		cxt.closePath();
-		cxt.fillStyle = '#00ff00';		// 连接杆
+		cxt.fillStyle = '#00ff00'; // 连接杆
 		cxt.fill();
-		cxt.fillRect(240,y+12,x-240,6);
+		cxt.fillRect(240, y + 12, x - 240, 6);
 
 		// 柱子
-		var gradient=cxt.createLinearGradient(240,0,246,0);
-		gradient.addColorStop("0","#ccc");
-		gradient.addColorStop("0.7","#555");
-		gradient.addColorStop("1.0","#999");
+		var gradient = cxt.createLinearGradient(240, 0, 246, 0);
+		gradient.addColorStop("0", "#ccc");
+		gradient.addColorStop("0.7", "#555");
+		gradient.addColorStop("1.0", "#999");
 		cxt.fillStyle = gradient;
-		cxt.fillRect(240,state.y-40,6,480);
+		cxt.fillRect(240, state.y - 40, 6, 480);
 
 		// 连接点
-		cxt.fillStyle = '#00ff00';	// 连接点 + 爪子
-		cxt.arc(243,this.y+15,5,0,2*Math.PI);
+		cxt.fillStyle = '#00ff00'; // 连接点 + 爪子
+		cxt.arc(243, this.y + 15, 5, 0, 2 * Math.PI);
 		cxt.fill();
-		
-		if ( this.hand != 0 ) drawcell(this.x,this.y,this.hand);
+
+		if (this.hand != 0) drawcell(this.x, this.y, this.hand);
 	}
-	this.halt = function(){
+	this.halt = function() {
 		arm.init();
 		state.draw();
 	}
-	this.done = function(v,i){					// 第(v,i)块指令执行完毕
+	this.done = function(v, i) { // 第(v,i)块指令执行完毕
 		log("[Done] " + v + ',' + i);
-		if ( v == undefined ) return ;
-		if ( runs.tasks[v][i+1] != 0 ) {
-			runs.run(v,i+1);				// 执行(v,i+1)
-		}else{								// 检查堆栈
-			var l = runs.stack.length-1;
-			if ( l >= 0 ){
+		if (v == undefined) return;
+		if (runs.tasks[v][i + 1] != 0) {
+			runs.run(v, i + 1); // 执行(v,i+1)
+		} else { // 检查堆栈
+			var l = runs.stack.length - 1;
+			if (l >= 0) {
 				var x = runs.stack[l][0];
 				var y = runs.stack[l][1];
 				log("[Back] " + x + ',' + y);
 				runs.stack.length--;
-				this.done(x,y);
-			}else{
-				runs.finish();			// finish
+				this.done(x, y);
+			} else {
+				runs.finish(); // finish
 			}
 		}
 	}
-	this.died = function(){
+	this.died = function() {
 		message("Destroyed!<hr>通过下方按钮“重新开始”")
 	}
-	this.right = function(v,ii){
+	this.right = function(v, ii) {
 		if (!this.running) return;
 		var i = 6;
-		while ( i>0 && state.box[this.r-1][i-1] != 0 ) i--;
-		if ( i==0 && this.hand != 0) {
+		while (i > 0 && state.box[this.r - 1][i - 1] != 0) i--;
+		if (i == 0 && this.hand != 0) {
 			this.died();
 			return;
 		}
-		this.goRight(state.x+conf.cell.x*i,v,ii);
+		this.goRight(state.x + conf.cell.x * i, v, ii);
 	}
-	this.left = function(v,i){			// 带入参数表示正在执行第(v,i)块指令
+	this.left = function(v, i) { // 带入参数表示正在执行第(v,i)块指令
 		if (!this.running) return;
-		this.x -= this.speed ;
-		if ( this.x > this.leftz ){
-			setTimeout('arm.left('+v+','+i +')',conf.Fz);
-		}else {
-			this.done(v,i);
+		this.x -= this.speed;
+		if (this.x > this.leftz) {
+			setTimeout('arm.left(' + v + ',' + i + ')', conf.Fz);
+		} else {
+			this.done(v, i);
 		}
 	}
-	this.up = function(v,i){
+	this.up = function(v, i) {
 		if (!this.running) return;
-		if ( this.r == 1 ) {
+		if (this.r == 1) {
 			this.died();
-			return ;
+			return;
 		}
-		this.y -= this.speed ;
-		if (this.y > (state.y+conf.cell.y*(this.r-2)*2) ) {
-			setTimeout('arm.up('+v+','+i+')',conf.Fz);
+		this.y -= this.speed;
+		if (this.y > (state.y + conf.cell.y * (this.r - 2) * 2)) {
+			setTimeout('arm.up(' + v + ',' + i + ')', conf.Fz);
 			//sleep(conf.Fz);this.up();
-		}else {
-			this.r --;
-			this.done(v,i);
+		} else {
+			this.r--;
+			this.done(v, i);
 		}
 	}
-	this.down = function(v,i){				
+	this.down = function(v, i) {
 		if (!this.running) return;
-		if ( this.r == 6 ) {
+		if (this.r == 6) {
 			this.died();
-			return ;
+			return;
 		}
-		this.y += this.speed ;
-		if ( this.y < ( state.y+conf.cell.y*(this.r*2))) {
-			setTimeout('arm.down('+v+','+i+')',conf.Fz);
+		this.y += this.speed;
+		if (this.y < (state.y + conf.cell.y * (this.r * 2))) {
+			setTimeout('arm.down(' + v + ',' + i + ')', conf.Fz);
 			//sleep(conf.Fz);this.down();
-		}else {
-			this.r ++ ;
-			this.done(v,i);
+		} else {
+			this.r++;
+			this.done(v, i);
 		}
 	}
-	this.goRight = function(x,v,ii){
+	this.goRight = function(x, v, ii) {
 		if (!this.running) return;
-		this.x 	+= this.speed ;
-		if ( this.x + (this.hand > 0)*conf.cell.x  < x ){
-			setTimeout('arm.goRight('+x+','+v+','+ii+')',conf.Fz);
+		this.x += this.speed;
+		if (this.x + (this.hand > 0) * conf.cell.x < x) {
+			setTimeout('arm.goRight(' + x + ',' + v + ',' + ii + ')', conf.Fz);
 			//sleep(conf.Fz);this.goRight(x);
-		}else if ( this.x + (this.hand > 0)*conf.cell.x  == x ){
-			
+		} else if (this.x + (this.hand > 0) * conf.cell.x == x) {
+
 			var i = 6;
-			while ( i>0 && state.box[this.r-1][i-1] != 0 ) i--;	// i <- first conf.cell	
-			
+			while (i > 0 && state.box[this.r - 1][i - 1] != 0) i--; // i <- first conf.cell	
+
 			// fall
-			if (i>0 && this.hand>0 ){
-				state.box[this.r-1][i-1] = this.hand;
+			if (i > 0 && this.hand > 0) {
+				state.box[this.r - 1][i - 1] = this.hand;
 				this.hand = 0;
-			}
-			else 
-			
+			} else
+
 			// catch
-			if (i<6 && this.hand==0 && state.box[this.r-1][i] != 0){
-				this.hand = state.box[this.r-1][i];
-				state.box[this.r-1][i] = 0;
+			if (i < 6 && this.hand == 0 && state.box[this.r - 1][i] != 0) {
+				this.hand = state.box[this.r - 1][i];
+				state.box[this.r - 1][i] = 0;
 			}
-			
-			this.left(v,ii);
-			setTimeout(checkAns,200);
+
+			this.left(v, ii);
+			setTimeout(checkAns, 200);
 		}
 	}
 
--- modules/runs.js
+++ modules/runs.js
@@ -26,149 +26,159 @@
 		stop()		强行终止机器
 	}
 */
-function RUNS(){
+function RUNS() {
 	that = this;
 	this.obj = eid("runs");
-	this.x = state.x + conf.cell.x*8;
+	this.x = state.x + conf.cell.x * 8;
 	this.y = state.y - conf.cell.y + cope.height * 0.7;
 	this.r = cope.width * 9;
 	this.c = cope.height * 6.8;
-	this.clear = function(){
-		this.tasks=[[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
-		this.ifs=[[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+	this.clear = function() {
+		this.tasks = [
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0]
+		];
+		this.ifs = [
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0]
+		];
 		this.stack = [];
 	}
 	this.clear();
-	this.init = function(xxx){
+	this.init = function(xxx) {
 		this.stop();
-		if (xxx == 1 ){
-			for ( i=0; i<4; i++){
-				for ( j=1; j<9; j++){
+		if (xxx == 1) {
+			for (i = 0; i < 4; i++) {
+				for (j = 1; j < 9; j++) {
 					// toolifs + tool
 					var newson = document.createElement('div');
 					newson.className = "toola";
 					// toolifs
 					var newson2 = document.createElement('div');
 					newson2.className = "toolifs";
-					newson2.setAttribute("copeid",i*8+j);
-					newson2.onclick = function(e){
-						runs.settoolifs(e,this.getAttribute("copeid"));
+					newson2.setAttribute("copeid", i * 8 + j);
+					newson2.onclick = function(e) {
+						runs.settoolifs(e, this.getAttribute("copeid"));
 					}
 					newson.appendChild(newson2);
 					// tool
 					newson2 = document.createElement('div');
 					newson2.className = 'tool';
-					newson2.setAttribute("copeid",i*8+j);
-					newson2.setAttribute('id', 'cope'+(i*8+j));
-					newson2.onclick = function(e){
-						runs.settool(e,this.getAttribute('copeid'));
+					newson2.setAttribute("copeid", i * 8 + j);
+					newson2.setAttribute('id', 'cope' + (i * 8 + j));
+					newson2.onclick = function(e) {
+						runs.settool(e, this.getAttribute('copeid'));
 					}
 					newson.appendChild(newson2);
 					this.obj.appendChild(newson);
-					if ( i==3 && j==5 ) break;
+					if (i == 3 && j == 5) break;
 				}
 			}
-			this.obj.style.left = this.x+cope.width + c.offsetLeft + 1;
+			this.obj.style.left = this.x + cope.width + c.offsetLeft + 1;
 			this.obj.style.top = this.y + c.offsetTop - cope.height * 0.7;
 			this.obj.style.width = cope.width * 8;
 			this.obj.style.display = 'none';
 		}
 	}
-	this.settool = function(e,v){
-		toolbar.show(e.clientX,e.clientY+10);
+	this.settool = function(e, v) {
+		toolbar.show(e.clientX, e.clientY + 10);
 		toolbar.settool(v);
 	}
-	this.settoolifs = function(e,v){
-		toolbar.show2(e.clientX,e.clientY+10);
+	this.settoolifs = function(e, v) {
+		toolbar.show2(e.clientX, e.clientY + 10);
 		toolbar.settool2(v);
 	}
-	this.draw = function(){
-		for ( i=0; i<4; i++){
-			for ( j=1; j<9; j++){
-				cope.draw(this.x+cope.width*j,this.y+cope.height*i*1.7,this.tasks[i][j-1],cope.width,cope.height);
-				if (this.ifs[i][j-1] != 0){
-					cope.drawifs(this.x+cope.width*j,this.y+cope.height*i*1.7,this.ifs[i][j-1]);
-				}else {
-					cope.drawifs(this.x+cope.width*j,this.y+cope.height*i*1.7,0);
+	this.draw = function() {
+		for (i = 0; i < 4; i++) {
+			for (j = 1; j < 9; j++) {
+				cope.draw(this.x + cope.width * j, this.y + cope.height * i * 1.7, this.tasks[i][j - 1], cope.width, cope.height);
+				if (this.ifs[i][j - 1] != 0) {
+					cope.drawifs(this.x + cope.width * j, this.y + cope.height * i * 1.7, this.ifs[i][j - 1]);
+				} else {
+					cope.drawifs(this.x + cope.width * j, this.y + cope.height * i * 1.7, 0);
 				}
-				if ( i==3 && j==5 ) break;
+				if (i == 3 && j == 5) break;
 			}
-			cope.draw(this.x,this.y+cope.height*i*1.7,i+4,cope.width,cope.height);
+			cope.draw(this.x, this.y + cope.height * i * 1.7, i + 4, cope.width, cope.height);
 		}
 		this.obj.style.display = 'block';
 	}
-	this.hide = function(){
+	this.hide = function() {
 		this.obj.style.display = 'none';
 	}
-	this.run = function(v,i){			//	运行(v,i)
+	this.run = function(v, i) { //	运行(v,i)
 		log("[Try ] " + v + "," + i);
-		if ( !arm.running ) return;			
-		if (this.tasks[v][i] == 0 ){		// 没有要执行的指令
+		if (!arm.running) return;
+		if (this.tasks[v][i] == 0) { // 没有要执行的指令
 			return;
 		}
-		if ( this.ifs[v][i] != 0 && this.ifs[v][i]!=arm.hand ){	//	条件不满足时跳过这块
-			arm.done(v,i);
+		if (this.ifs[v][i] != 0 && this.ifs[v][i] != arm.hand) { //	条件不满足时跳过这块
+			arm.done(v, i);
 			return;
 		}
-		coststep ++;
-		switch( this.tasks[v][i] ){
-			case 1 :
-				arm.right(v,i);
+		coststep++;
+		switch (this.tasks[v][i]) {
+			case 1:
+				arm.right(v, i);
 				break;
-			case 2 :
-				arm.up(v,i);
+			case 2:
+				arm.up(v, i);
 				break;
-			case 3 :
-				arm.down(v,i);
+			case 3:
+				arm.down(v, i);
 				break;
-			case 4 :
-				this.stack.push([v,i]);
-				this.run(0,0);
+			case 4:
+				this.stack.push([v, i]);
+				this.run(0, 0);
 				break;
-			case 5 :
-				this.stack.push([v,i]);
-				this.run(1,0);
+			case 5:
+				this.stack.push([v, i]);
+				this.run(1, 0);
 				break;
-			case 6 :
-				this.stack.push([v,i]);
-				this.run(2,0);
+			case 6:
+				this.stack.push([v, i]);
+				this.run(2, 0);
 				break;
-			case 7 :
-				this.stack.push([v,i]);
-				this.run(3,0);
+			case 7:
+				this.stack.push([v, i]);
+				this.run(3, 0);
 				break;
 		}
 	}
-	this.start = function(){
+	this.start = function() {
 		this.store.save();
 		initLevel(Mission);
 		this.store.load();
 		arm.running = true;
 		costime = 0;
 		coststep = 0;
-		this.run(0,0);
+		this.run(0, 0);
 	}
-	this.finish = function(){
+	this.finish = function() {
 		clearFlash();
 		arm.halt();
 	}
-	this.stop = function(){
+	this.stop = function() {
 		this.finish();
 	}
-	this.store={
-		save:function(){
-			localStorage.setItem("armgo_runs_"+Mission,JSON.stringify(that.tasks));
-			localStorage.setItem("armgo_runs_ifs_"+Mission,JSON.stringify(that.ifs));
+	this.store = {
+		save: function() {
+			localStorage.setItem("armgo_runs_" + Mission, JSON.stringify(that.tasks));
+			localStorage.setItem("armgo_runs_ifs_" + Mission, JSON.stringify(that.ifs));
 			console.log(localStorage);
 		},
-		load:function(){
-			t = "armgo_runs_"+Mission;
-			res = eval( "(" + localStorage.getItem(t) + ")" );
-			if ( res != null){
+		load: function() {
+			t = "armgo_runs_" + Mission;
+			res = eval("(" + localStorage.getItem(t) + ")");
+			if (res != null) {
 				that.tasks = res;
-				t = "armgo_runs_ifs_"+Mission;
-				res = eval( "(" + localStorage.getItem(t) + ")" );
-				if ( res != null ){
+				t = "armgo_runs_ifs_" + Mission;
+				res = eval("(" + localStorage.getItem(t) + ")");
+				if (res != null) {
 					that.ifs = res;
 				}
 			}
