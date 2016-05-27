791d0fec785bb52e17a7d881abe1b4bc1108b752
zhanghuiren <zhrlnt@gmail.com>
2016-05-27 23:38:47 +0800

    部署脚本

---
+++ deploy.php
@@ -0,0 +1,16 @@
+<?php
+
+function run($cmds)
+{
+    foreach ((array)$cmds as $cmd) {
+        $output = [];
+        $return = null;
+        exec($cmd, $output, $return);
+
+        foreach ($output as $line) {
+            echo $line . "\n";
+        }
+    }
+}
+
+run('git pull');
